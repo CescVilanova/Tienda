f0cb3eb2f9665df54e41bc9d8bd21b126287b9d3
CescVilanova <vfrancesc@gmail.com>
2014-02-27 10:25:41 +0000

    Base de datos cambiada a mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224173239) do
+ActiveRecord::Schema.define(version: 20140226153442) do
 
   create_table "products", force: true do |t|
     t.string   "name"
